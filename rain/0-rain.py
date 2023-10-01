d46bf110a6aeb8572eb1662a5f8061b3efeea530
kevin-gram <k.nyiringan@alustudent.com>
2023-10-01 21:58:59 +0300

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""
-solving the rain question
-"""
+"""Solution for rain"""
+
+
 def rain(walls):
-
-    if not walls:
+    """Calculate how many square units of water
+    will be retained after it rains"""
+    if len(walls) == 0:
         return 0
-
-    left, right = 0, len(walls) - 1
-    left_max, right_max = 0, 0
     water = 0
-
-    while left < right:
-        if walls[left] <= walls[right]:
-            if walls[left] >= left_max:
-                left_max = walls[left]
-            else:
-                water += left_max - walls[left]
-            left += 1
-        else:
-            if walls[right] >= right_max:
-                right_max = walls[right]
-            else:
-                water += right_max - walls[right]
-            right -= 1
-
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
     return water
+
+
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
